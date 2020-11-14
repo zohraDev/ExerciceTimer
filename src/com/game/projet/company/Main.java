3b52125f8eab97bd434fd3bc9e8211f129b81937
zohraDev <bach.zora@hotmail.fr>
2020-11-14 15:14:36 +0100

    Deuxième méthode création d'une classe qui heritera de Timer Task

--- src/com/game/projet/company/Main.java
+++ src/com/game/projet/company/Main.java
@@ -11,11 +11,15 @@ public class Main {
         Timer chron = new Timer();
         chron.schedule( new  TimerTask(){
 
-                int compteur =0 ; // pour arrêter le chronomètre
+                int compteur =10 ; // pour arrêter le chronomètre
             @Override
             public void run() {
 
                 System.out.println("Hi gys");
+                compteur--;
+                if(compteur==0){
+                    cancel();// la méthode qui arrête le chrono.
+                }
 
             }
         },1000,1000);// boucle infinie. delay en milliesecondes.
